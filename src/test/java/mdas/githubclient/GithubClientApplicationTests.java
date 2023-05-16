c7789d74eb94568a364b277351a4a849ffcf3219
lGutierrezF <93689570+lGutierrezF@users.noreply.github.com>
2023-05-16 21:21:08 +0200

    feat: initial commit.
    Basic structure of the application.

---
+++ src/test/java/mdas/githubclient/GithubClientApplicationTests.java
@@ -0,0 +1,13 @@
+package mdas.githubclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
