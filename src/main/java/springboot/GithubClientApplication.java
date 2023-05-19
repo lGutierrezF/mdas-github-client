28d98ecd9299100c9552c4255c228947d5591875
lauragutierrez <laura.gutierrez@hexad.de>
2023-05-19 22:46:48 +0200

    feat: added springboot controller.

--- src/main/java/springboot/GithubClientApplication.java
+++ src/main/java/springboot/GithubClientApplication.java
@@ -1,13 +1,25 @@
 package springboot;
 
+import mdas.github.client.infrastructure.GithubClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+import java.io.IOException;
+
+
+@ComponentScan(basePackages = {"mdas.github.client"})
 @SpringBootApplication
 public class GithubClientApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(GithubClientApplication.class, args);
+		var githubClient = new GithubClient();
+		//var user = githubClient.getUser("lGutierrezF");
+		//var repos =  githubClient.getUserRepos("octocat");
+		//var comments =  githubClient.getUserPullRequestComments("octocat","2016-09-27");
+		//var comments =  githubClient.getUserExecutedPullRequests("octocat","2016-09");
+		var commits =  githubClient.getUserCommits("octocat","octocat.github.io","11","2015");
 	}
 
 }
