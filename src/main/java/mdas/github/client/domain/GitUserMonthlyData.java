20a520d338619197b8eda7031a55a998f4d5538f
lauragutierrez <laura.gutierrez@hexad.de>
2023-05-21 18:11:36 +0200

    feat: added git user monthly stats aggregation.

--- src/main/java/mdas/github/client/domain/GitUserMonthlyData.java
+++ src/main/java/mdas/github/client/domain/GitUserMonthlyData.java
@@ -3,10 +3,44 @@ package mdas.github.client.domain;
 public class GitUserMonthlyData {
     private String login;
     private String date;
-    public GitUserMonthlyData(String login) {
+    private GitUserReviewedPullRequests reviewedPullRequests;
+    private GitUserExecutedPullRequests executedPullRequests;
+    private GitUserPullRequestComments pullRequestComments;
+    private GitCommitStats gitCommitStats;
+
+    public GitUserMonthlyData(String login, String date, GitUserReviewedPullRequests reviewedPullRequests,
+                              GitUserExecutedPullRequests executedPullRequests,
+                              GitUserPullRequestComments pullRequestComments, GitCommitStats gitCommitStats) {
         this.login = login;
+        this.date = date;
+        this.reviewedPullRequests = reviewedPullRequests;
+        this.executedPullRequests = executedPullRequests;
+        this.pullRequestComments = pullRequestComments;
+        this.gitCommitStats = gitCommitStats;
     }
+
     public String getLogin() {
         return login;
     }
+
+    public String getDate() {
+        return date;
+    }
+
+    public GitUserReviewedPullRequests getReviewedPullRequests() {
+        return reviewedPullRequests;
+    }
+
+    public GitUserExecutedPullRequests getExecutedPullRequests() {
+        return executedPullRequests;
+    }
+
+    public GitUserPullRequestComments getPullRequestComments() {
+        return pullRequestComments;
+    }
+
+    public GitCommitStats getGitCommitStats() {
+        return gitCommitStats;
+    }
+
 }
