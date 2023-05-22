1db70bd2958ebb8b523f6930a0aadf8e974a0c80
lauragutierrez <laura.gutierrez@hexad.de>
2023-05-22 14:17:00 +0200

    feature: added a constructor and data access methods.

--- src/main/java/mdas/github/client/domain/GitUser.java
+++ src/main/java/mdas/github/client/domain/GitUser.java
@@ -2,4 +2,17 @@ package mdas.github.client.domain;
 
 public class GitUser {
     private String login;
+
+    public GitUser(String login) {
+        this.login = login;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
 }
