c8b0864e77c7f98f9fbd59f18680c47d37fa3857
lauragutierrez <laura.gutierrez@hexad.de>
2023-05-22 14:16:28 +0200

    feature: added a commiter field.

--- src/main/java/mdas/github/client/domain/GitUserCommit.java
+++ src/main/java/mdas/github/client/domain/GitUserCommit.java
@@ -3,10 +3,12 @@ package mdas.github.client.domain;
 public class GitUserCommit {
     private String sha;
     private GitCommitStats stats;
+    private GitUser committer;
 
-    public GitUserCommit(String sha, GitCommitStats stats) {
+    public GitUserCommit(String sha, GitCommitStats stats, GitUser commiter) {
         this.sha = sha;
         this.stats = stats;
+        this.committer = commiter;
     }
     public String getSha() {
         return sha;
@@ -16,5 +18,11 @@ public class GitUserCommit {
     }
     public void setStats(GitCommitStats stats) {
         this.stats = stats;
+    }
+    public String getCommiter(){
+        return this.committer.getLogin();
+    }
+    public void setCommiter(GitUser commiter) {
+        this.committer = commiter;
     }
 }
